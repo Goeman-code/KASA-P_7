d3ea93ee89e5fc005e4fd0294f126cbdf02a959c
Antoine <antoine.goeman@gmail.com>
2023-04-27 01:44:41 +0200

    banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,5 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import k from '../assets/k.svg'
+import a1 from '../assets/a1.svg'
+import s from '../assets/s.svg'
+import a2 from '../assets/a2.svg'
+import '../styles/banner.css'
+
 export default function banner() {
     return (
-        <h1>BANNER ...</h1>
+        <div className="banner">
+            <div className="banner-logo">
+                <img src={k} alt="k"></img>
+                <img src={a1} alt="a1"></img>
+                <img src={s} alt="s"></img>
+                <img src={a2} alt="a2"></img>
+            </div>
+            <div className="banner-link">
+                <Link to="/" className="banner-link-links hover-underline-animation">Accueil</Link>
+                <Link to="/apropos" className="banner-link-links hover-underline-animation">À propos</Link>
+            </div>
+        </div>
     )
 }
