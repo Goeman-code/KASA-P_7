3fd0e96ef4b4744f538faa6f61c05e0fa4fae320
Antoine <antoine.goeman@gmail.com>
2023-05-04 20:19:05 +0200

    starting fichelogement: slider

--- src/pages/fichelogement.js
+++ src/pages/fichelogement.js
@@ -1,5 +1,48 @@
-export default function fichelogement() {
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Logements from '../logements.json'
+import Collapse from '../components/collapse'
+import arrowRight from '../assets/arrow-right.svg'
+import arrowLeft from '../assets/arrow-left.svg'
+import '../styles/fichelogement.css'
+
+export default function Fichelogement() {
+
+    const currentId = useParams()
+    const logement = Logements.find(logement => logement.id = currentId )
+
+
+    const lastIndex = logement.pictures.length - 1
+    const [image, setImage] = useState(0)
+
+    const handleLeftArrowClick = () => {
+        if (image === 0) {
+            setImage(lastIndex)
+            console.log(image)
+        } else {
+            setImage(image - 1)
+            console.log(image)
+        }
+    }
+
+    const handleRightArrowClick = () => {
+        if (image === lastIndex) {
+            setImage(0)
+            console.log(image)
+        } else {
+            setImage(image + 1)
+            console.log(image)
+        }
+    }
+
+    let currentImage = logement.pictures[image]
+    console.log(currentImage)
+
     return (
-        <h1>FICHELOGEMENT ...</h1>
+        <div className="slider">
+            <img src={arrowLeft} alt="arrow" className="arrow" onClick={handleLeftArrowClick}></img>
+            <img src={arrowRight} alt="arrow" className="arrow" onClick={handleRightArrowClick}></img>
+        </div>
     )
+
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom"
 import Card from '../components/cards'
 import Logements from '../logements.json'
 import '../styles/home.css'
+import { useParams } from "react-router";
 
 export default function home() {
-    let stringLog = JSON.stringify(Logements)
-    let validLog = JSON.parse(stringLog)
-    console.log(validLog)
-
     return (
         <div>
             <div className="section-1">
                 <p className="section-1-texte">Chez vous, partout et ailleurs</p>
             </div>
             <div className="home-cards">
-            {validLog.map((logement) => (
+            {Logements.map((logement) => (
                     <Card title={`${logement.title}`} cover={`${logement.cover}`} id={`${logement.id}`} />
                 ))}
             </div>
